25e93dd144758fffce0baf71dad12b916b64d26c
Adit Modi <adit.modi24@gmail.com>
2020-09-05 21:25:35 +0530

    Update handler.js

--- backend/handler.js
+++ backend/handler.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports.listTodos = async event => {
-  const todos = ["Wake up at 4AM", "Workout", "Eat breakfast"];
+  const todos = ["Wake up at 4AM", "Workout", "Eat breakfast","Attend Lec","Sleep"];
   return {
     statusCode: 200,
     body: JSON.stringify({ todos }, null, 2)
